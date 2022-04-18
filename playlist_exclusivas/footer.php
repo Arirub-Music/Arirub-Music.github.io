f07b2479b6790244d0c167b39b8d55995652abe8
AriRubDam <arirub08@gmail.com>
2022-04-18 11:28:32 -0500

    prueba 2

--- playlist_exclusivas/footer.php
+++ playlist_exclusivas/footer.php
@@ -1,4 +1,8 @@
-<script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+		<script src="https://jailbreak-update.github.io/jquery/jquery-3.3.1.min.js"></script>	
+		<script src="js/sweetalert.js"></script>
+
+        <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>
         <script src="js/particulas.js"></script>
 
         <div id="donate-button-container">
@@ -16,12 +20,12 @@
             }).render('#donate-button');
             </script>
             </div>
-            
-        <input type="checkbox" id="btn-menu">
+
+            <input type="checkbox" id="btn-menu">
         <div class="container-menu">
             <div class="cont-menu">
                 <nav>
-                    <a href="">Inicio</a>
+                    <a href="/index.html">Inicio</a>
                     <a id="btn0">Acerca de Mi</a>
                     <a id="btn1">Categorias</a>
                 </nav>
@@ -29,23 +33,7 @@
             </div>
         </div>
 
-        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-		<script src="https://jailbreak-update.github.io/jquery/jquery-3.3.1.min.js"></script>	
-		<script src="js/sweetalert.js"></script>
-
-        <script src="js/main.js"></script>
-
-        <div class="aviso-cookies" id="aviso-cookies">
-            <img src="img/cookie.svg" alt="galleta" class="galleta">
-            <h3 class="titulo">Cookies</h3>
-            <p class="parrafo">Utilizamos cookies propias y de terceros para mejorar nuestros servicios.</p>
-            <button class="boton" id="btn-aceptar-cookies">De acuerdo</button>
-            <a class="enlace" href="aviso-cookies.html">Aviso de Cookies</a>
-        </div>
-
-        <div class="fondo-aviso-cookies" id="fondo-aviso-cookies"></div>
-
-
-        <script src="js/aviso-cookies.js"></script>
+        <script src="/js/main.js"></script>
+            
 </body>
 </html>
