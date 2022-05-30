70e65a66d6046309bdbd4738a0d8aa3362481d76
AriRubDam <arirub08@gmail.com>
2022-05-29 22:26:53 -0500

    Update block.js

--- js/block.js
+++ js/block.js
@@ -1,1 +1,17 @@
-var _0x9900=["\x77\x68\x69\x63\x68","\x6B\x65\x79\x43\x6F\x64\x65","\x63\x74\x72\x6C\x4B\x65\x79","\x70\x72\x65\x76\x65\x6E\x74\x44\x65\x66\x61\x75\x6C\x74","\x73\x74\x6F\x70\x50\x72\x6F\x70\x61\x67\x61\x74\x69\x6F\x6E","\x45\x72\x72\x6F\x72\x21","\x65\x72\x72\x6F\x72","\x66\x69\x72\x65","\x48\x61\x20\x70\x72\x65\x73\x69\x6F\x6E\x61\x64\x6F\x20\x6C\x61\x73\x20\x74\x65\x63\x6C\x61\x73\x20\x43\x74\x72\x6C\x20\x2B\x20\x53","\x6C\x6F\x67","\x65\x76\x65\x6E\x74","\x61\x6C\x6C","\x6C\x61\x79\x65\x72\x73","\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x42\x79\x49\x64","\x63\x61\x70\x74\x75\x72\x65\x45\x76\x65\x6E\x74\x73","\x6F\x6E\x6D\x6F\x75\x73\x65\x64\x6F\x77\x6E","\x6F\x6E\x6D\x6F\x75\x73\x65\x75\x70","\x6F\x6E\x63\x6F\x6E\x74\x65\x78\x74\x6D\x65\x6E\x75","\x72\x65\x74\x75\x72\x6E\x20\x66\x61\x6C\x73\x65"];onkeydown= (_0xc811x1)=>{let _0xc811x2=_0xc811x1[_0x9900[0]]|| _0xc811x1[_0x9900[1]];if(_0xc811x1[_0x9900[2]]){_0xc811x1[_0x9900[3]]();_0xc811x1[_0x9900[4]]();if(_0xc811x2=== 85){Swal[_0x9900[7]]({title:_0x9900[5],icon:_0x9900[6]})};if(_0xc811x2=== 83){console[_0x9900[9]](_0x9900[8])}}};function desabilitar(){if(event[_0x9900[2]]){switch(window[_0x9900[10]][_0x9900[1]]){case 67:;case 86:event[_0x9900[1]]= 0;return false}}}function disableIE(){if(document[_0x9900[11]]){return false}}function disableNS(_0xc811x1){if(document[_0x9900[12]]|| (document[_0x9900[13]]&&  !document[_0x9900[11]])){if(_0xc811x1[_0x9900[0]]== 2|| _0xc811x1[_0x9900[0]]== 3){return false}}}if(document[_0x9900[12]]){document[_0x9900[14]](Event.MOUSEDOWN);document[_0x9900[15]]= disableNS}else {document[_0x9900[16]]= disableNS;document[_0x9900[17]]= disableIE};document[_0x9900[17]]=  new Function(_0x9900[18])
+var _0x9900=["\x77\x68\x69\x63\x68","\x6B\x65\x79\x43\x6F\x64\x65","\x63\x74\x72\x6C\x4B\x65\x79","\x70\x72\x65\x76\x65\x6E\x74\x44\x65\x66\x61\x75\x6C\x74","\x73\x74\x6F\x70\x50\x72\x6F\x70\x61\x67\x61\x74\x69\x6F\x6E","\x45\x72\x72\x6F\x72\x21","\x65\x72\x72\x6F\x72","\x66\x69\x72\x65","\x48\x61\x20\x70\x72\x65\x73\x69\x6F\x6E\x61\x64\x6F\x20\x6C\x61\x73\x20\x74\x65\x63\x6C\x61\x73\x20\x43\x74\x72\x6C\x20\x2B\x20\x53","\x6C\x6F\x67","\x65\x76\x65\x6E\x74","\x61\x6C\x6C","\x6C\x61\x79\x65\x72\x73","\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x42\x79\x49\x64","\x63\x61\x70\x74\x75\x72\x65\x45\x76\x65\x6E\x74\x73","\x6F\x6E\x6D\x6F\x75\x73\x65\x64\x6F\x77\x6E","\x6F\x6E\x6D\x6F\x75\x73\x65\x75\x70","\x6F\x6E\x63\x6F\x6E\x74\x65\x78\x74\x6D\x65\x6E\x75","\x72\x65\x74\x75\x72\x6E\x20\x66\x61\x6C\x73\x65"];
+onkeydown= (_0xc811x1)=>{
+    let _0xc811x2=_0xc811x1[_0x9900[0]]|| _0xc811x1[_0x9900[1]];
+    if(_0xc811x1[_0x9900[2]]){_0xc811x1[_0x9900[3]]();_0xc811x1[_0x9900[4]]();
+        if(_0xc811x2=== 85){Swal[_0x9900[7]]({title:_0x9900[5],icon:_0x9900[6]})};
+        if(_0xc811x2=== 83){console[_0x9900[9]](_0x9900[8])}}};function desabilitar(){
+            if(event[_0x9900[2]]){
+                switch(window[_0x9900[10]][_0x9900[1]]){
+                    case 67:;
+                    case 86:event[_0x9900[1]]= 0;
+                    return false}}}function disableIE(){
+                        if(document[_0x9900[11]])
+                        {return false}}function disableNS(_0xc811x1){
+                            if(document[_0x9900[12]]|| (document[_0x9900[13]]&&  !document[_0x9900[11]])){
+                                if(_0xc811x1[_0x9900[0]]== 2|| _0xc811x1[_0x9900[0]]== 3){return false}}}
+                                if(document[_0x9900[12]]){document[_0x9900[14]](Event.MOUSEDOWN);document[_0x9900[15]]= disableNS
+                                }else {document[_0x9900[16]]= disableNS;document[_0x9900[17]]= disableIE};document[_0x9900[17]]=  new Function(_0x9900[18])
